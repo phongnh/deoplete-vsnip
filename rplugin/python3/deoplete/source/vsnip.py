a5278e08852b06f2fbfb60d7297077f746f86da2
Phong Nguyen <nhphong1406@gmail.com>
2023-07-13 21:49:00 +0700

    Update snippets candidate

--- rplugin/python3/deoplete/source/vsnip.py
+++ rplugin/python3/deoplete/source/vsnip.py
@@ -1,4 +1,5 @@
 import json
+import re
 from deoplete.source.base import Base
 
 class Source(Base):
@@ -6,11 +7,20 @@ class Source(Base):
         Base.__init__(self, vim)
 
         self.name = 'vsnip'
-        self.mark = '[vsnip]'
+        self.mark = '[v]'
         self.rank = 1000
         self.input_pattern = r'\w\+$'
         self.min_pattern_length = 1
         self.vars = {}
 
     def gather_candidates(self, context):
-        return self.vim.call('vsnip#get_complete_items', context['bufnr'])
+        snippets = []
+        for item in self.vim.call('vsnip#get_complete_items', context['bufnr']):
+            snippets.append({
+                'word': item['word'],
+                'abbr': item['abbr'],
+                'menu': re.sub('^\[.*\]', self.mark, item['menu']),
+                'info': "\n".join(json.loads(item['user_data'])['vsnip']['snippet']),
+                'dup': item['dup']
+            })
+        return snippets
